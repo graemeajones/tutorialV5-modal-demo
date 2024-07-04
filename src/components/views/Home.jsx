9ace4e60f0c91a810379aa1e67d74bfd172dedad
Graeme Jones <48164351+graemeajones@users.noreply.github.com>
2024-07-04 13:27:50 +0100

    Update Home.jsx

--- src/components/views/Home.jsx
+++ src/components/views/Home.jsx
@@ -7,17 +7,13 @@ function Home() {
     <>
       <h1>Content</h1>
       <h2>Covers</h2>
-      <h3>
-        Refactoring the Fetch API
-      </h3>
-      <h3>
-        Specifying the backend URL once
-      </h3>
-      <h3>
-        Building a reusable useLoad hook to simplify reports and forms
-      </h3>
+      <h3>Building a Modal component</h3>
+      <h3>Opening a form within a modal</h3>
+      <h3>Building alert, confirmation, and error notification components using modals</h3>
       <h2>Assumes</h2>
-      <h3>You have watched the Post Form Demo video</h3>
+      <h3>
+        You have watched the <it>useContext</it> Demo video
+      </h3>
     </>
   );
 }
